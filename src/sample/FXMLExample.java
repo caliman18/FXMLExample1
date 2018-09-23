7d170450407ee8d536d64f40c53884d9a2404ab4
David <caliman18@hotmail.com>
2018-09-23 09:35:21 -0400

    Initial commit

---
+++ src/sample/FXMLExample.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FXMLExample extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(FXMLExample.class.getResource("fxml_example.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
